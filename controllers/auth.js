76115b2f9d487074371fc09888141662587e516e
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2021-12-16 22:19:35 +0100

    Añadido hasheo de contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,58 @@
 
 const { response } = require('express'); 
+const Usuario = require('../models/Usuario');
+const bcrypt = require('bcryptjs');
+const { generarJWT } = require('../helpers/jwt');
 
-const registerUser = (req, res = response ) => {
+const registerUser = async ( req, res = response ) => {
 
     const { email, name, password, organizationType } = req.body;
+
+    try {
+        // Verificar que el email es único
+        const usuario = await Usuario.findOne({ email });
+
+        if ( usuario ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User with this email already exists'
+            });
+        }
+
+        // Crear usuario con el modelo
+        const dbUser = new Usuario( req.body );
+
+        // Hashear la contraseña
+        const salt = bcrypt.genSaltSync();
+        dbUser.password = bcrypt.hashSync( password, salt );
+
+        // Generar JWT
+        const token = await generarJWT( dbUser.id, name );
+        // Crear usuario de DB
+        await dbUser.save();
+
+        // Generar respuesta exitosa
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name,
+            email,
+            organizationType,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Wrong Credentials'
+        })
+    }
+
+
     
-    return res.json({
-        ok: true,
-        msg: 'Crear usuario /register'
-    })
+    
+    
 }
 
 const loginUser = (req, res = response ) => {
