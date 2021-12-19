e9cd3dc6d127e12a282f0c0636a25e058d611cbc
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2021-12-19 23:46:29 +0100

    Inicio implementacion modelos

--- models/Usuario.js
+++ models/Usuario.js
@@ -21,4 +21,33 @@ const UsuarioSchema = Schema({
     }
 });
 
+
+
+/*
+
+export interface Project {
+    title: string,
+    projectId: string,
+    ownerId: string,
+    publishedDate: number,
+    deadLine: number,
+    techSet: string[],
+    shortExplanation: string,
+    state: string,
+    bid: number
+}
+
+export interface User {
+    name: string,
+    userId: string,
+    email: string,
+    password: string,
+    verified: boolean,
+    projectsPublished: Project[],
+    admin: boolean,
+    typeOfInstitution: string
+}
+
+*/
+
 module.exports = model('Usuario', UsuarioSchema);
